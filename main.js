4275bd6ec3696f8caa9172dbd1b087eb5558569f
Mercedes <mcarballalfr@gmail.com>
2020-06-26 14:51:45 +0200

    update conditional

--- main.js
+++ main.js
@@ -4,25 +4,36 @@ const updateButton = document.querySelector(".js-update");
 const moodButton = document.querySelector(".js-mood");
 const title = document.querySelector(".js-title");
 const main = document.querySelector(".js-main");
+const mainImpar = document.querySelector(".js-main-impar");
 
 function handleButtonClick(event) {
   /* console.log(event.target);
   console.log(moodButton.value); */
   title.innerHTML = moodButton.value;
 
-  function aleatorio(minimo, maximo) {
-    const result = Math.round(Math.random() * (maximo - minimo) + minimo);
-    console.log(result);
-    /* return result; */
+  console.log(aleatorio(0, 100));
+}
 
-    if (result % 2 === 0) {
+function aleatorio(minimo, maximo) {
+  const result = Math.round(Math.random() * (maximo - minimo) + minimo);
+  console.log(result);
+  /* return result; */
+
+  if (result % 2 === 0) {
+    return (
+      main.classList.add("js-main"), main.classList.remove("js-main-impar")
+    );
+  } else {
+    return (
+      main.classList.remove("js-main"), main.classList.add("js-main-impar")
+    );
+  }
+}
+updateButton.addEventListener("click", handleButtonClick);
+
+/* if (result % 2 === 0) {
       return (main.style.backgroundColor = "#ffcc00");
     } else {
       return (main.style.backgroundColor = "#ff9900");
     }
-  }
-
-  console.log(aleatorio(0, 100));
-}
-
-updateButton.addEventListener("click", handleButtonClick);
+  } */
