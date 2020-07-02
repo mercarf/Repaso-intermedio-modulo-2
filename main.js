87c93adb36ad2ed35cf40f2cb47a15e5e5f7c2b7
Mercedes <mcarballalfr@gmail.com>
2020-07-02 20:29:57 +0200

    margins update

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
-"use strict";
+'use strict';
 
-const updateButton = document.querySelector(".js-update");
-const moodButton = document.querySelector(".js-mood");
-const title = document.querySelector(".js-title");
-const main = document.querySelector(".js-main");
-const mainImpar = document.querySelector(".js-main-impar");
+const updateButton = document.querySelector('.js-update');
+const moodButton = document.querySelector('.js-mood');
+const title = document.querySelector('.js-title');
+const main = document.querySelector('.js-main');
+const mainImpar = document.querySelector('.js-main-impar');
 
-function handleButtonClick(event) {
+function handleButtonClick(ev) {
   /* console.log(event.target);
   console.log(moodButton.value); */
+  ev.preventDefault();
   title.innerHTML = moodButton.value;
 
-  console.log(aleatorio(0, 100));
+  console.log(randomNumber(0, 100));
 }
 
-function aleatorio(minimo, maximo) {
+function randomNumber(minimo, maximo) {
   const result = Math.round(Math.random() * (maximo - minimo) + minimo);
   console.log(result);
   /* return result; */
 
   if (result % 2 === 0) {
     return (
-      main.classList.add("js-main"), main.classList.remove("js-main-impar")
+      main.classList.add('js-main'), main.classList.remove('js-main-impar')
     );
   } else {
     return (
-      main.classList.remove("js-main"), main.classList.add("js-main-impar")
+      main.classList.remove('js-main'), main.classList.add('js-main-impar')
     );
   }
 }
-updateButton.addEventListener("click", handleButtonClick);
+updateButton.addEventListener('click', handleButtonClick);
 
 /* if (result % 2 === 0) {
       return (main.style.backgroundColor = "#ffcc00");
