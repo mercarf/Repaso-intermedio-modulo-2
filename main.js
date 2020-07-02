58252a06aec725b4ef41e8bb5d50b0481cb6d29f
Mercedes <mcarballalfr@gmail.com>
2020-07-02 20:41:17 +0200

    clean code

--- main.js
+++ main.js
@@ -6,35 +6,39 @@ const title = document.querySelector('.js-title');
 const main = document.querySelector('.js-main');
 const mainImpar = document.querySelector('.js-main-impar');
 
+/* Funcion manejadora que se ejecuta al hacer click en el botón update */
 function handleButtonClick(ev) {
-  /* console.log(event.target);
-  console.log(moodButton.value); */
   ev.preventDefault();
-  title.innerHTML = moodButton.value;
+  updateFace();
+  getRandomNumber();
+  updateBackground();
 
-  console.log(randomNumber(0, 100));
+  //Número aleatorio que hay que adivinar
+  console.log(getRandomNumber());
+}
+//Función que imprime en pantalla la carita seleccionada
+function updateFace() {
+  title.innerHTML = moodButton.value;
 }
 
-function randomNumber(minimo, maximo) {
-  const result = Math.round(Math.random() * (maximo - minimo) + minimo);
-  console.log(result);
-  /* return result; */
-
-  if (result % 2 === 0) {
-    return (
-      main.classList.add('js-main'), main.classList.remove('js-main-impar')
-    );
+/* Función que actualiza el color de fondo en función del si el 
+número aleatorio es par o impar */
+function updateBackground() {
+  const randomNumber = getRandomNumber();
+  main.classList.remove('js-main');
+  main.classList.remove('js-main-impar');
+  if (randomNumber % 2 === 0) {
+    main.classList.add('js-main');
   } else {
-    return (
-      main.classList.remove('js-main'), main.classList.add('js-main-impar')
-    );
+    main.classList.add('js-main-impar');
   }
 }
-updateButton.addEventListener('click', handleButtonClick);
 
-/* if (result % 2 === 0) {
-      return (main.style.backgroundColor = "#ffcc00");
-    } else {
-      return (main.style.backgroundColor = "#ff9900");
-    }
-  } */
+/* Funcion que genera un número aleatorio */
+const getRandomNumber = function () {
+  const randomNumber = parseInt(Math.random() * 100);
+  return randomNumber;
+};
+
+//Listener
+updateButton.addEventListener('click', handleButtonClick);
